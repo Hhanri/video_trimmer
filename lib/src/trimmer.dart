24ff43078e33544608557d21130708a282f692a1
HUYNH Henry <huynhhenry.dev@gmail.com>
2024-09-15 10:43:59 +0200

    [min_gpl] replaced ffmpeg_kit_flutter https witth min_gpl version

--- lib/src/trimmer.dart
+++ lib/src/trimmer.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'dart:io';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
-import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
-import 'package:ffmpeg_kit_flutter/return_code.dart';
-import 'package:path/path.dart';
 
+import 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_min_gpl/ffmpeg_kit_config.dart';
+import 'package:ffmpeg_kit_flutter_min_gpl/return_code.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:video_player/video_player.dart';
 import 'package:video_trimmer/src/utils/file_formats.dart';
